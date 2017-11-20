83fb6ea38d621db45e7b4827ab5d4f1160c80c6e
Hugo Bravo Diaz <hbravod@gamma10.aulas.gsyc.urjc.es>
2017-11-20 10:31:16 +0100

    cliente

--- client.py
+++ client.py
@@ -5,23 +5,36 @@ Programa cliente que abre un socket a un servidor
 """
 
 import socket
+import sys
+
+
+if len(sys.argv) < 3:
+    print("Usage: python3 client.py method receiver@IP:SIPport")
 
 # Cliente UDP simple.
 
-# Dirección IP del servidor.
-SERVER = 'localhost'
-PORT = 6001
-
 # Contenido que vamos a enviar
-LINE = '¡Hola mundo!'
+LINE = sys.argv[2]
+
+# Dirección IP del servidor.
+SERVER = LINE.split('@')[1].split(':')[0]
+PORT = int(LINE.split('@')[1].split(':')[1])
+method = sys.argv[1]
+    
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     my_socket.connect((SERVER, PORT))
 
-    print("Enviando: " + LINE)
-    my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
+    if method == "INVITE":
+        my_socket.send(bytes('INVITE sip:'+LINE+' SIP/2.0\r\n', 'utf-8') + 
+                             b'\r\n')
+
+    if method == "BYE":
+        my_socket.send(bytes('BYE sip:'+LINE+' SIP/2.0\r\n', 'utf-8') + 
+                             b'\r\n')
+
     data = my_socket.recv(1024)
 
     print('Recibido -- ', data.decode('utf-8'))
